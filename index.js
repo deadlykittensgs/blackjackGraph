c3244ec34c51d6ff3caedd6a670f1c914619ccec
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-05-11 18:15:38 -0500

    count added value needs to be fixed

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let playerwins = document.getElementById("playerwins")
+let dealerwins = document.getElementById("dealerwins")
+
+
+
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 const ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
 let theDealerWon = 0
@@ -30,25 +35,25 @@ let usersCardTwo = getCards()
 /// add a function to turn the strings to a number
 
 console.log(usersCardOne)
-if (usersCardOne.inclues("ace")){
+// if (usersCardOne.inclues("ace")){
 
-  usersCardOne = ace()
-}
+//   usersCardOne = ace()
+// }
 
-if (usersCardOne.inclues("King")|| usersCardOne.inclues("Queen")|| usersCardOne.inclues("Jack")){
+// if (usersCardOne.inclues("King")|| usersCardOne.inclues("Queen")|| usersCardOne.inclues("Jack")){
 
-  usersCardOne = 10
-}
-// ----------------  card two
-if (usersCardTwo.inclues("ace")){
+//   usersCardOne = 10
+// }
+// // ----------------  card two
+// if (usersCardTwo.inclues("ace")){
 
-  usersCardTwo = ace()
-}
+//   usersCardTwo = ace()
+// }
 
-if (usersCardTwo.inclues("King")|| usersCardTwo.inclues("Queen")|| usersCardTwo.inclues("Jack")){
+// if (usersCardTwo.inclues("King")|| usersCardTwo.inclues("Queen")|| usersCardTwo.inclues("Jack")){
 
-  usersCardTwo = 10
-}
+//   usersCardTwo = 10
+// }
 
 
 let usersTotal = usersCardOne + usersCardTwo 
@@ -88,7 +93,8 @@ if (dealer > user || user > 21 ) {
 if (dealer < user || dealer || 21) {
   theUserTotal++
 }
-
+dealerwins.innerText = theDealerWon
+playerwins.innerText =theUserTotal
 }
 
 
